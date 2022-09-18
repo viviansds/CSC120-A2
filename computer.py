7feac73eacd1b7dd3bae4fffe9073189b84b1a60
Vivian Wei <vwei@smith.edu>
2022-09-18 16:51:53 -0400

    added functions in both computer and  oo_resale.py

--- computer.py
+++ computer.py
@@ -4,7 +4,39 @@ class Computer:
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self,description: str,
+                    processor_type: str,
+                    hard_drive_capacity: int,
+                    memory: int,
+                    operating_system: str,
+                    year_made: int,
+                    price: int):
+        self.description = description
+        self.processor_type = processor_type
+        self.hard_drive_capacity = hard_drive_capacity
+        self.memory = memory
+        self.operating_system = operating_system
+        self.year_made =  year_made
+        self.price = price
+    def create_computer(description: str,
+                processor_type: str,
+                hard_drive_capacity: int,
+                memory: int,
+                operating_system: str,
+                year_made: int,
+                price: int):
+        return {'description': description,
+                'processor_type': processor_type,
+                'hard_drive_capacity': hard_drive_capacity,
+                'memory': memory,
+                'operating_system': operating_system,
+                'year_made': year_made,
+             'price': price
+      }
+# new_computer=Computer.create_computer("Mac Pro (Late 2013)",
+#     "3.5 GHc 6-Core Intel Xeon E5",
+#     1024, 64,
+#     "macOS Big Sur", 2013, 1500)
+# print(new_computer)
 
     # What methods will you need?
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,10 +1,58 @@
-class ResaleShop:
+from typing import Dict, Union, Optional
+from computer import Computer
 
+itemID = 0
+inventory : Dict[int, Dict[str, Union[str, int, bool]]] = {}
+# Import a few useful containers from the typing module
+
+
+class ResaleShop:
     # What attributes will it need?
 
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
-
+    def __init__(self):
+        self.inventory : Dict[int, Dict[str, Union[str, int, bool]]] = {}
+        self.itemID:int = 0
     # What methods will you need?
+    def buy(computer):
+        global itemID
+        itemID += 1 # increment itemID
+        # inventory[itemID].append(computer)
+        inventory[itemID] = computer
+        return itemID
+
+    def print_inventory():
+    # If the inventory is not empty
+        if inventory:
+        # For each item
+            for item_id in inventory:
+            # Print its details
+                print(f'Item ID: {itemID} : {inventory[itemID]}')
+        else:
+            print("No inventory to display.")
+
+    def refurbish(item_id: int, new_os: Optional[str]):
+        print(inventory)
+        if item_id in inventory:
+            computer = inventory[item_id] # locate the computer
+            if inventory[item_id]['year_made'] < 2000:
+                computer[item_id]['price'] = 0 # too old to sell, donation only
+            elif inventory[item_id]['year_made'] < 2012:
+                computer[item_id]['price'] = 250 # heavily-discounted price on machines 10+ years old
+            elif inventory[item_id]['year_made'] < 2018:
+                computer[item_id]['price'] = 550 # discounted price on machines 4-to-10 year old machines
+            else:
+                computer[item_id]['price'] = 1000 # recent stuff
+
+        if new_os is not None:
+            inventory['operating_system'] = new_os # update details after installing new OS
+        else:
+            print("Item", item_id, "not found. Please select another item to refurbish.")
+
+    def sell(item_id: int):
+        if item_id in inventory:
+            del inventory[item_id]
+            print("Item", item_id, "sold!")
+        else: 
+            print("Item", item_id, "not found. Please select another item to sell.")
