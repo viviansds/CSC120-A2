7bb32a8e631af4e98b6605db5c8735b04eed3e32
Vivian Wei <vwei@smith.edu>
2022-09-19 22:05:36 -0400

    Wrapped comments

--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -1,29 +1,33 @@
 # Import a few useful containers from the typing module
 from typing import Dict, Union, Optional
-""" This class file takes in an empty nested dictionary named inventory that 
+""" This class file takes in an empty nested dictionary named inventory that
     store information about the computer and its corresponding itemID.
-    ResaleShop have five functions: buy(), print_inventory(),refurbish(), 
+    ResaleShop have five functions: buy(), print_inventory(),refurbish(),
     update_price(), and sell() that modifies the inventory.
 """
 class ResaleShop:
-    # What attributes will it need?
-    # How will you set up your constructor?
+    # What attributes will it need? How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    """initialize ResaleShop class with empty inventory and itemID that begins at 0"""
+    """initialize ResaleShop class with empty inventory and itemID that begins
+    at 0
+    """
     def __init__(self):
         self.inventory : Dict[int, Dict[str, Union[str, int, bool]]] = {}
         self.itemID:int = 0
 
     # What methods will you need?
-    """Takes in a instance of the class, a dictionary of computer infos and pair with 
-        incremented itemID and returns the current itemID as a integer"""
+    """Takes in a instance of the class, a dictionary of computer infos and pair
+    with incremented itemID and returns the current itemID as a integer
+    """
     def buy(self,computer:dict):
         self.itemID += 1 # increment itemID
         #assign computer information as values associated with the key 'itemID'.
         self.inventory[self.itemID] = computer 
         return self.itemID
 
-    """Takes in a instance of the class, print itemID and its corresponding value(computer infos)"""
+    """Takes in a instance of the class, print itemID and its corresponding 
+    value(computer infos)
+    """
     def print_inventory(self):
         # If the inventory is not empty
         if self.inventory:
@@ -34,18 +38,21 @@ class ResaleShop:
         else:
             print("No inventory to display.")
 
-    """ Takes in a instance of the class, a integer indicating the specific item_id and an optional 
-        string for the new_os.Update a computer's price base on its year made and its operating system if 
-        given. 'item_id' is different from 'itemID' because item itemID are incremented and only reflect 
-        the last item added to the inventory.
-        """
+    """ Takes in a instance of the class, a integer indicating the specific
+        item_id and an optional string for the new_os.Update a computer's price
+        base on its year made and its operating system if given. 'item_id' is
+        different from 'itemID' because item itemID are incremented and only
+        reflect the last item added to the inventory.
+    """
     def refurbish(self,item_id: int, new_os: Optional[str]=None): 
         if item_id in self.inventory:
             #check to see if the computer is in the inventory dictionary
             if item_id in self.inventory:
-                #assign "computer" as the name for the values associated with the key "itemID" in the nested dictionary called ""inventory
+                #assign "computer" as the name for the values associated with
+                #the key "itemID" in the nested dictionary called ""inventory
                 computer = self.inventory[item_id]
-                #access and modify the computer dictionary that is created in computer.py 
+                #access and modify the computer dictionary that is created in
+                #computer.py 
                 if int(computer["year_made"]) < 2000:
                     computer["price"] = 0 # too old to sell, donation only
                 elif int(computer["year_made"]) < 2012:
@@ -54,20 +61,23 @@ class ResaleShop:
                     computer["price"] = 550 # discounted price on machines 4-to-10 year old machines
                 else:
                     computer["price"] = 1000 # recent stuff
-                #If a new_os is assigned in main.py and is placed as the arguments of the refurbish() function, update the value associated with the key 'operating_system'
+                #If a new_os is assigned in main.py and is placed as the
+                #arguments of the refurbish() function, update the value
+                #associated with the key 'operating_system'
                 if new_os is not None:
                     computer['operating_system'] = new_os # update details after installing new OS
         else:
             print("Item", item_id, "not found. Please select another item to refurbish.")
 
-    """This is a function that takes in an item_id and a new price, updates any arbitrary 
-        assigned price to a computer in the inventory, regardless of the year made. If it is 
-        not in the inventory, prints error message.
-        """
+    """This is a function that takes in an item_id and a new price, updates any
+    arbitrary assigned price to a computer in the inventory, regardless of
+    the year made. If it is not in the inventory, prints error message.
+    """
     def update_price(self,item_id: int, new_price: int):
          #check to see if the computer is in the inventory dictionary
         if item_id in self.inventory:
-            #access and modifies a nested dictionary by assigning a new value for the key "price"
+            #access and modifies a nested dictionary by assigning a new value
+            #for the key "price"
             self.inventory[item_id]["price"] = new_price
         else:
             print("Item", item_id, "not found. Cannot update price.")
